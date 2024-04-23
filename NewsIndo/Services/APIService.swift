0bac941600df8aaa36546d4563e351c9ee9f6871
AffanDN <affan03nusli@gmail.com>
2024-04-23 09:54:14 +0700

    API Service fetch news

---
+++ NewsIndo/Services/APIService.swift
@@ -0,0 +1,34 @@
+//
+//  APIService.swift
+//  NewsIndo
+//
+//  Created by Macbook Pro on 23/04/24.
+//
+
+import Foundation
+import Alamofire
+
+class APIService {
+    static let shared = APIService()
+    
+    // suapaya gabisa dibuat di file lain
+    private init() {}
+    
+    // returnnya newsarticle
+    func fetchNews() async throws -> [NewsArticle] {
+        guard let url = URL(string: Constant.newsUrl) else {throw
+            URLError(.badURL)}
+        
+        let news = try await withCheckedThrowingContinuation { continuation in
+            AF.request(url).responseDecodable(of: News.self) { response in
+                switch response.result {
+                case .success(let newsResponse) :
+                    continuation.resume(returning: newsResponse.data)
+                case.failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+        return []
+    }
+}
