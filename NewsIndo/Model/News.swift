bf82088736bf56146fee1f2638407a553a9ef1ad
AffanDN <affan03nusli@gmail.com>
2024-04-23 09:24:27 +0700

    Mapping data news model

---
+++ NewsIndo/Model/News.swift
@@ -0,0 +1,32 @@
+//
+//  News.swift
+//  NewsIndo
+//
+//  Created by Macbook Pro on 23/04/24.
+//
+
+import Foundation
+
+struct News: Decodable {
+    let messages: String
+        let total: Int
+        let data: [NewsArticle]
+}
+
+// MARK: - NewsArticle
+struct NewsArticle: Codable, Identifiable {
+    var id: String {
+        link
+    }
+    let creator, title: String
+    let link: String
+    let author: String
+    let categories: [String]
+    let isoDate, description: String
+    let image: Image
+}
+
+// MARK: - Image
+struct Image: Codable {
+    let small, medium, large, extraLarge: String
+}
